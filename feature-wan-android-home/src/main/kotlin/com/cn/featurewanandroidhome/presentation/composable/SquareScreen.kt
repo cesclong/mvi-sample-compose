3de7476d36ca05ce7c186c454400164a1a7d33e8
Long, Cesc <cesclong@163.com>
2022-11-03 21:56:55 +0800

    square 列表

--- feature-wan-android-home/src/main/kotlin/com/cn/featurewanandroidhome/presentation/composable/SquareScreen.kt
+++ feature-wan-android-home/src/main/kotlin/com/cn/featurewanandroidhome/presentation/composable/SquareScreen.kt
@@ -1,17 +1,182 @@
 package com.cn.featurewanandroidhome.presentation.composable
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.cesc.commonmodel.Article
+import org.jetbrains.annotations.NotNull
 import org.koin.androidx.compose.koinViewModel
 
 
 @Composable
-internal fun SquareScreen(navController: NavController, viewModel : SquareViewModel = koinViewModel()) {
-    Column(modifier = Modifier.fillMaxSize()) {
-       Text(text = "square") 
+internal fun SquareScreen(
+    navController: NavController,
+    viewModel: SquareViewModel = koinViewModel()
+) {
+
+    val uiState by viewModel.uiState.collectAsState()
+
+    DisposableEffect(key1 = Unit) {
+        viewModel.sendIntent(SquareIntent.Fetch(0))
+        onDispose { }
     }
+
+    when {
+        uiState.isLoading -> {
+            LoadView()
+        }
+
+        uiState.isError -> ErrorView()
+
+        uiState.datas.isNotEmpty() -> {
+            SquareView(uiState.datas)
+        }
+    }
+
+}
+
+@Composable
+internal fun LoadView() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        CircularProgressIndicator(
+            modifier = Modifier.align(Alignment.Center)
+        )
+    }
+
+}
+
+@Composable
+internal fun ErrorView(msg: String = "error") {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Text(
+            text = msg,
+            modifier = Modifier.align(Alignment.Center)
+        )
+    }
+
+}
+
+@Composable
+internal fun SquareView(articles: List<Article>) {
+    LazyColumn {
+
+        items(articles) {
+            SquareItemView(article = it)
+        }
+    }
+
+}
+
+@Composable
+internal fun SquareItemView(article: Article) {
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(160.dp)
+            .padding(10.dp), shape = RoundedCornerShape(10.dp),
+        elevation = 5.dp,
+        backgroundColor = MaterialTheme.colors.primary
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(10.dp)
+        ) {
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Text(
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    text = article.shareUser,
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxWidth()
+                        .wrapContentHeight()
+                )
+
+                Text(
+                    text = article.niceDate,
+                    fontSize = 14.sp
+                )
+            }
+
+            Text(text = article.title, fontSize = 18.sp)
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp)
+
+            ) {
+                LabelTextButton(text = article.chapterName)
+                LabelTextButton(text = article.superChapterName)
+
+            }
+
+        }
+
+    }
+}
+
+
+@Composable
+fun LabelTextButton(
+    text: String,
+    modifier: Modifier = Modifier,
+    cornerValue: Dp = 25.dp / 2,
+    onClick: (() -> Unit)? = null
+) {
+    Text(
+        text = text,
+        modifier = modifier
+            .height(25.dp)
+            .clip(shape = RoundedCornerShape(cornerValue))
+            .background(
+                color = Color(0XFF3489FF)
+            )
+            .padding(
+                horizontal = 10.dp,
+                vertical = 3.dp
+            )
+            .clickable {
+                onClick?.invoke()
+            },
+        fontSize = 13.sp,
+        textAlign = TextAlign.Center,
+        color = Color.White,
+        overflow = TextOverflow.Ellipsis,
+        maxLines = 1
+    )
 }
--- feature-wan-android-home/src/main/kotlin/com/cn/featurewanandroidhome/presentation/composable/SquareViewModel.kt
+++ feature-wan-android-home/src/main/kotlin/com/cn/featurewanandroidhome/presentation/composable/SquareViewModel.kt
@@ -1,9 +1,88 @@
 package com.cn.featurewanandroidhome.presentation.composable
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.cesc.commonmodel.Article
 import com.cn.featurewanandroidhome.domain.SquareUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class SquareViewModel(
     private val useCase: SquareUseCase
-)  : ViewModel(){
+) : ViewModel() {
+    private val channel = Channel<SquareIntent>(Channel.UNLIMITED)
+
+    private val _uiState = MutableStateFlow(SquareUiState())
+
+    val uiState: StateFlow<SquareUiState>
+        get() = _uiState
+
+    init {
+        channel.receiveAsFlow()
+            .onEach(::handleIntent)
+            .launchIn(viewModelScope)
+
+    }
+
+    private fun handleIntent(intent: SquareIntent) {
+        channel.trySend(intent)
+    }
+
+    fun sendIntent(intent: SquareIntent) {
+        when (intent) {
+            is SquareIntent.Fetch -> {
+                fetchSquareData(intent.page)
+            }
+        }
+    }
+
+    private fun fetchSquareData(page: Int) {
+        updateState {
+            it.copy(isLoading = true)
+        }
+        viewModelScope.launch {
+            val respond = withContext(Dispatchers.IO) {
+                runCatching {
+                    useCase.getSquareArticles(page)
+                }
+            }
+            respond.fold(
+                onSuccess = { model ->
+                    updateState {
+                        it.copy(isLoading = false, isError = false, datas = model.articles)
+                    }
+                },
+                onFailure = {
+                    updateState {
+                        it.copy(isLoading = false, isError = true)
+                    }
+
+                }
+            )
+
+        }
+    }
+
+    private fun updateState(handler: (SquareUiState) -> SquareUiState) {
+        _uiState.tryEmit(handler(_uiState.value))
+    }
 }
+
+sealed class SquareIntent {
+    data class Fetch(val page: Int) : SquareIntent()
+
+}
+
+
+data class SquareUiState(
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val datas: List<Article> = emptyList()
+)
